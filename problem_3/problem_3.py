bb94546c54f542925ea99fc0dbe65bc980822fd7
VRass-pixel <VitRas21@highgateschool.org.uk>
2024-12-04 14:00:09 +0000

    some more

--- problem_3/problem_3.py
+++ problem_3/problem_3.py
@@ -1,6 +1,6 @@
 import re
 
-with open(problem_3/input_3.txt) as file:
+with open int('problem_3/input_3.txt', 'r') as file:
     file_contents = file.read()
-
-mul_statements = re.findall('/mul\(\d+,\d+\)/gm', file_contents)
+    mul_statements = re.findall('mul\((\d+),(\d+)\)', file_contents)
+    mul_free = [(x * y) for x, y in file]
