e90464d7e4d82ebaec9ac8a46739f8a945252290
VRass-pixel <VitRas21@highgateschool.org.uk>
2024-12-09 22:11:04 +0000

    1

--- problem_3/problem_3.py
+++ problem_3/problem_3.py
@@ -1,6 +1,8 @@
 import re
 
-with open int('problem_3/input_3.txt', 'r') as file:
+with open ('problem_3/input_3.txt', 'r') as file:
     file_contents = file.read()
     mul_statements = re.findall('mul\((\d+),(\d+)\)', file_contents)
-    mul_free = [(x * y) for x, y in file]
+    mul_results = [int(x) * int(y) for x, y in mul_statements]
+    total_sum = sum(mul_results)
+    print(f"Total sum of multiplication results: {total_sum}")
